2a2258f09b4d3b412bc9b01f05ce3ef73b0b5036
Aleksei <kovrigin@inetris.ru>
2022-05-01 20:23:22 +0300

    first result

--- src/Lib.php
+++ src/Lib.php
@@ -6,7 +6,23 @@ namespace classmap;
 
 class Lib
 {
-	function example (){
-		return "3";
+	public $allowSymbols = ["(", ")", "\n", "\t", "\r"];
+	function example ($string): bool
+	{
+		$arrRes = [];
+		$arr = str_split($string);
+		foreach ($arr as $key => $value) {
+			if (in_array($value, $this->allowSymbols)) {
+				$arrRes[$value]++;
+			} else {
+				throw new \InvalidArgumentException('found symbols'.$value);
+			}
+		}
+
+		if (count($arrRes["("]) == count($arrRes[")"])){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
